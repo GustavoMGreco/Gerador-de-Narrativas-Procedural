dccf609de641e301d9e508234c6200701fee9fb6
Gustavo Monteiro Greco <gustavomonteirogreco@gmail.com>
2026-03-11 16:51:15 -0300

    Conexão do PrismaClient com a rota /quests para retornar um actor do BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { ActorsModule } from './actors/actors.module';
 import { QuestsModule } from './quests/quests.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ActorsModule, QuestsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ActorsModule, 
+    QuestsModule, 
+    PrismaModule
+  ],
   controllers: [],
   providers: [],
 })
--- src/quests/quests.module.ts
+++ src/quests/quests.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { QuestsController } from './quests.controller';
 import { QuestsService } from './quests.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [QuestsController],
-  providers: [QuestsService],
+  providers: [QuestsService]
 })
 export class QuestsModule {}
